796d3d6bfc72e5f5ddadfa5b233e82521e6003ef
HeiniEmilia <heini.ilmarinen@gmail.com>
2013-09-12 16:24:19 +0300

    luokkakaavio lisatty, tuntikirjanpito ja projektikansio paivitetty

---
+++ Treeniloki/src/Urheilulaji.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Heini
+ */
+public class Urheilulaji {
+    private String nimi;
+    private int tunnit;
+    private int minuutit;
+    
+    public Urheilulaji(String nimi){
+        this.nimi = nimi;
+        this.tunnit = 0;
+        this.minuutit = 0;
+    }
+    
+    public String haeAika(){
+        if(this.minuutit>9){
+            return this.tunnit + ":" + this.minuutit;
+        }
+        return this.tunnit + ":0" + this.minuutit;
+    }
+    
+    public void lisaaAika(int lisaaTunnit, int lisaaMinuutit){
+        int minuutitYhteensa = lisaaMinuutit + this.minuutit;
+        if(minuutitYhteensa < 60){
+            this.minuutit = minuutitYhteensa;
+            this.tunnit = this.tunnit + lisaaTunnit;
+        } else {
+            int tuntejaMinuuteista = minuutitYhteensa/60;
+            int minuutitMinuuteista = lisaaMinuutit - (tuntejaMinuuteista*60);
+            this.tunnit = this.tunnit + lisaaTunnit + tuntejaMinuuteista;
+            this.minuutit = this.minuutit + minuutitMinuuteista;
+        }
+    }
+    
+    public boolean suurempiAika(Urheilulaji verrattava){
+        if (this.tunnit>verrattava.tunnit || (this.tunnit==verrattava.tunnit && this.minuutit>verrattava.minuutit)){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean samatAjat(Urheilulaji verrattava){
+        if(this.tunnit==verrattava.tunnit && this.minuutit==verrattava.minuutit){
+            return true;
+        }
+        return false;
+    }
+    
+    public String toString(){
+        return this.nimi + " "+ this.haeAika();
+    }
+}
---
+++ Treeniloki/test/UrheilulajiTest.java
@@ -0,0 +1,133 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author Heini
+ */
+public class UrheilulajiTest {
+    
+    Urheilulaji laji1;
+    Urheilulaji laji2;
+    
+    public UrheilulajiTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        laji1 = new Urheilulaji("hiihto");
+        laji2 = new Urheilulaji("juoksu");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    //@Test
+    //public void hello() {}
+    
+    @Test
+    public void konstruktoriAsettaaNimenJaAjanOikein(){
+        String vastaus = laji1.toString();
+        
+        assertEquals("hiihto 0:00", vastaus);
+    }
+    
+    @Test
+    public void lisaaTunnitOikein(){
+        laji1.lisaaAika(2,0);
+        String vastaus = laji1.toString();
+        
+        assertEquals("hiihto 2:00", vastaus);
+    }
+    
+    @Test
+    public void lisaaAlleKuusikymmentaMinuuttiaOikein(){
+        laji1.lisaaAika(0,50);
+        String vastaus = laji1.toString();
+        
+        assertEquals("hiihto 0:50", vastaus);
+    }
+    
+    @Test
+    public void lisaaTasanKuusikymmentaMinuuttiaOikein(){
+        laji1.lisaaAika(0,60);
+        String vastaus = laji1.toString();
+        
+        assertEquals("hiihto 1:00", vastaus);
+    }
+    
+    @Test
+    public void lisaaYliKuusikymmentaMinuuttiaOikein(){
+        laji1.lisaaAika(0,50);
+        laji1.lisaaAika(0,45);
+        String vastaus = laji1.toString();
+        
+        assertEquals("hiihto 1:35", vastaus);
+    }
+    
+    @Test
+    public void suurempiAikaPalauttaaTrue(){
+        laji1.lisaaAika(0,50);
+        laji2.lisaaAika(0,45);
+        boolean vastaus = laji1.suurempiAika(laji2);
+        
+        assertEquals(true, vastaus);
+    }
+        
+    @Test
+    public void pienempiAikaPalauttaaFalse(){
+        laji1.lisaaAika(0,50);
+        laji2.lisaaAika(0,45);
+        boolean vastaus = laji2.suurempiAika(laji1);
+        
+        assertEquals(false, vastaus);
+    }
+    
+    @Test
+    public void samatAjatPalauttaaFalse(){
+        laji1.lisaaAika(0,50);
+        laji2.lisaaAika(0,50);
+        boolean vastaus = laji2.suurempiAika(laji1);
+        
+        assertEquals(false, vastaus);
+    }
+    
+    @Test
+    public void tunnistaaSamatAjat(){
+        laji1.lisaaAika(0,50);
+        laji2.lisaaAika(0,50);
+        boolean vastaus = laji2.samatAjat(laji1);
+        
+        assertEquals(true, vastaus);
+    }
+    
+    @Test
+    public void tunnistaaEriAjat(){
+        laji1.lisaaAika(0,40);
+        laji2.lisaaAika(0,50);
+        boolean vastaus = laji2.samatAjat(laji1);
+        
+        assertEquals(false, vastaus);
+    }
+}
